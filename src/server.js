90e43c1ebcd9fe7cded5ab80704f960749ff4d53
Nicholas Schamberg <schamberg.nicholas@gmail.com>
2020-09-14 01:27:58 +0300

    Show warning if clustering is used on non-Linux platform

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ const { toString, toLowerCase } = require('./utils/string')
 const { forEach } = require('./utils/object')
 const REQUEST_EVENT = 'request'
 
+const {worker} = require('cluster')
+const {threadId } = require('worker_threads')
+
+if (require('os').platform !== 'linux' && ( (worker && worker.id) || threadId === 1)) {
+  console.log('Be aware that uWebSockets.js clustering only works on Linux and depends on its kernel features. See <https://github.com/uNetworking/uWebSockets.js/issues/214#issuecomment-547589050> for more info') // https://github.com/uNetworking/uWebSockets.js/issues/214#issuecomment-547589050
+}
+
 module.exports = (config = {}) => {
   let handler = (req, res) => {
     res.statusCode = 404
