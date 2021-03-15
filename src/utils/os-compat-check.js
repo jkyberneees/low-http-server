b79fed162d5252aeef348b370b6f2b107af3e802
Nicholas Schamberg <schamberg.nicholas@gmail.com>
2021-03-15 13:40:50 +0300

    Linting

--- src/utils/os-compat-check.js
+++ src/utils/os-compat-check.js
@@ -2,11 +2,9 @@ const { worker } = require('cluster')
 let threadId
 try {
   threadId = require('worker_threads').threadId
-}
-catch (err) {
+} catch (err) {
   threadId = 0
 }
-
 
 if (require('os').platform !== 'linux' && ((worker && worker.id === 1) || threadId === 1)) {
   console.log('Be aware that uWebSockets.js clustering only works on Linux and depends on its kernel features. See <https://github.com/uNetworking/uWebSockets.js/issues/214#issuecomment-547589050> for more info') // https://github.com/uNetworking/uWebSockets.js/issues/214#issuecomment-547589050
