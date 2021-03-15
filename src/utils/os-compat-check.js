6c906152b73112a768c6d2630f2978dd80565bfc
Nicholas Schamberg <schamberg.nicholas@gmail.com>
2021-03-15 13:38:33 +0300

    Fix os-compat-check on Node.js w/o worker_threads

--- src/utils/os-compat-check.js
+++ src/utils/os-compat-check.js
@@ -1,5 +1,12 @@
 const { worker } = require('cluster')
-const { threadId } = require('worker_threads')
+let threadId
+try {
+  threadId = require('worker_threads').threadId
+}
+catch (err) {
+  threadId = 0
+}
+
 
 if (require('os').platform !== 'linux' && ((worker && worker.id === 1) || threadId === 1)) {
   console.log('Be aware that uWebSockets.js clustering only works on Linux and depends on its kernel features. See <https://github.com/uNetworking/uWebSockets.js/issues/214#issuecomment-547589050> for more info') // https://github.com/uNetworking/uWebSockets.js/issues/214#issuecomment-547589050
