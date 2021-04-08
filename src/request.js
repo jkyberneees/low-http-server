9c2d22d62f734e2db2761e737f6ae75dd1c732de
Nicholas Schamberg <schamberg.nicholas@gmail.com>
2021-04-08 12:09:32 +0300

    linting

--- src/request.js
+++ src/request.js
@@ -1,38 +1,38 @@
 const { Readable } = require('stream')
 
 class HttpRequest extends Readable {
-  constructor(uRequest) {
-    super();
+  constructor (uRequest) {
+    super()
 
-    const q = uRequest.getQuery();
-    this.req = uRequest;
-    this.url = uRequest.getUrl() + (q ? "?" + q : "");
-    this.method = uRequest.getMethod().toUpperCase();
-    this.statusCode = null;
-    this.statusMessage = null;
-    this.body = {};
-    this.headers = {};
-    this.socket = {};
+    const q = uRequest.getQuery()
+    this.req = uRequest
+    this.url = uRequest.getUrl() + (q ? '?' + q : '')
+    this.method = uRequest.getMethod().toUpperCase()
+    this.statusCode = null
+    this.statusMessage = null
+    this.body = {}
+    this.headers = {}
+    this.socket = {}
 
     uRequest.forEach((header, value) => {
-      this.headers[header] = value;
-    });
+      this.headers[header] = value
+    })
   }
 
-  getRawHeaders() {
-    const raw = [];
+  getRawHeaders () {
+    const raw = []
     forEach(this.headers, (header, value) => {
-      raw.push(header, value);
-    });
-    return raw;
+      raw.push(header, value)
+    })
+    return raw
   }
 
-  getRaw() {
-    return this.req;
+  getRaw () {
+    return this.req
   }
 
-  _read(size) {
-    return this.slice(0, size);
+  _read (size) {
+    return this.slice(0, size)
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const uWS = require('uWebSockets.js')
 const EventEmitter = require('events')
 require('./utils/os-compat-check')
 const REQUEST_EVENT = 'request'
-const ArrayBufferDecoder = new TextDecoder("utf-8");
+const ArrayBufferDecoder = new TextDecoder('utf-8')
 
 const HttpRequest = require('./request')
 const HttpResponse = require('./response')
@@ -27,16 +27,16 @@ module.exports = (config = {}) => {
     const reqWrapper = new HttpRequest(req)
     const resWrapper = new HttpResponse(res, uServer)
 
-    reqWrapper.res = resWrapper;
-    resWrapper.req = reqWrapper;
+    reqWrapper.res = resWrapper
+    resWrapper.req = reqWrapper
 
     reqWrapper.socket = {
       destroy: function () {
         return resWrapper.res.end()
       },
-      get remoteAddress() { 
-        /* 
-          required by fastify and other frameworks to get client IP. 
+      get remoteAddress () {
+        /*
+          required by fastify and other frameworks to get client IP.
           Returns either IPv4 or IPv6
         */
         let remote = resWrapper.res.getRemoteAddressAsText()
@@ -68,16 +68,15 @@ module.exports = (config = {}) => {
   uServer._date = new Date().toUTCString()
   const timer = setInterval(() => (uServer._date = new Date().toUTCString()), 1000)
 
-
   class Facade extends EventEmitter {
-    constructor() {
+    constructor () {
       super()
 
       const oldThisOn = this.on.bind(this)
       const oldThisOnce = this.once.bind(this)
 
       this.once = function (eventName, listener) {
-        return oldThisOnce(eventName, listener)    
+        return oldThisOnce(eventName, listener)
       }
 
       this.on = function (eventName, listener) {
@@ -85,58 +84,59 @@ module.exports = (config = {}) => {
           handler = listener
           return
         }
-        return oldThisOn(eventName, listener)    
+        return oldThisOn(eventName, listener)
       }
     }
+
     close (cb) {
       clearInterval(timer)
       uWS.us_listen_socket_close(uServer._socket)
       if (!cb) return
       return cb()
     }
+
     start (host, port, cb) {
       let args
-      const callbackFunction = function(socket) {
+      const callbackFunction = function (socket) {
         uServer._socket = socket
-  
+
         if (cb) cb(socket)
       }
       if (host, port, cb) {
-        args = [host,port,callbackFunction]
+        args = [host, port, callbackFunction]
       }
       if (!cb && (!port || typeof port === 'function')) {
-        cb = port;
-        port = host;
-        args = [port,callbackFunction]
+        cb = port
+        port = host
+        args = [port, callbackFunction]
       }
       return uServer.listen(...args)
     }
+
     listen (host, port, cb) {
       if (typeof host === 'object') {
-        const listenOptions = host;
+        const listenOptions = host
         port = listenOptions.port
         cb = listenOptions.cb
         host = listenOptions.host
         return this.start(host, port, socket => {
           uServer._socket = socket
-    
+
           if (cb) cb(socket)
         })
       }
       if ((!port || typeof port === 'function') && !cb) {
-        cb = port;
-        port = host;
+        cb = port
+        port = host
         return this.start(port, cb)
-      }
-      else {
+      } else {
         return this.start(host, port, cb)
       }
-      
     }
-    get uwsApp() {
+
+    get uwsApp () {
       return uServer
     }
-    
   }
 
   const facade = new Facade()
